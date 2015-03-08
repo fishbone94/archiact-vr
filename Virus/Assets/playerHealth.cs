0be5f6cd6206fa0fff30839ce0b84f08ebdbc4f5
vgleung <vgleung@sfu.ca>
2015-03-08 08:59:00 -0700

    added health to player and takes damage from enemy collision

---
+++ Virus/Assets/playerHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerHealth : MonoBehaviour {
+	public int startingHealth = 100;
+	public int currentHealth;
+
+	bool isDead = false;
+	bool damaged = false;
+
+	void Awake () {
+		currentHealth = startingHealth;
+	}
+
+	//just code for later use
+	public void takeDamage (int amount) {
+		damaged = true;
+		currentHealth -= amount;
+
+		if (currentHealth <= 0 && !isDead) {
+			death();
+		}
+	}
+
+	public void death() {
+		isDead = true;
+	}
+	
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.tag == "Enemy") {
+			takeDamage(5);
+		}
+	}
+}
