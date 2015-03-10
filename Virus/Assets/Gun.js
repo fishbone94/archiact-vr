9d66d12ad365d900474e5fdff3e3678535d034a4
SkyZero <EdwardChao@live.com>
2015-03-09 18:31:35 -0700

    lol

---
+++ Virus/Assets/Gun.js
@@ -0,0 +1,20 @@
+﻿var Bullet : Rigidbody;
+var Spawn : Transform;
+var BulletSpeed: float = 1000;
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetButtonDown("Fire1")){
+	Fire();
+	
+	}
+	}
+	
+	function Fire(){
+	var bullet1 : Rigidbody = Instantiate(Bullet,Spawn.position,Spawn.rotation);
+	bullet1.AddForce(transform.forward * BulletSpeed);
+	}
+	
---
+++ Virus/Assets/TestPathing1.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var startPosition : Vector3 = Vector3(-0,50,0);    //The starting position in world space
+ var endPosition : Vector3 = Vector3(0,100,0);    //The ending position in world space
+ var bending : Vector3 = Vector3.up;                //Bend factor (on all axes)
+ var timeToTravel : float = 5.0;                //The total time it takes to move from start- to end position
+ 
+ function Start () {
+     MoveToPosition();
+ }
+ 
+ function MoveToPosition () {
+     var timeStamp : float = Time.time;
+     while (Time.time<timeStamp+timeToTravel) {
+         var currentPos : Vector3 = Vector3.Lerp(startPosition, endPosition, (Time.time - timeStamp)/timeToTravel);
+         
+         currentPos.x += bending.x*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
+         currentPos.y += bending.y*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
+         currentPos.z += bending.z*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
+         
+         transform.position = currentPos;
+         
+         yield;
+     }
+ }
