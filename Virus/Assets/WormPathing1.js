993a9c46ae9700698422922c64fcce6f19b1c060
SkyZero <EdwardChao@live.com>
2015-03-11 20:48:42 -0700

    wormpath

--- Virus/Assets/WormPathing1.js
+++ Virus/Assets/WormPathing1.js
@@ -1,11 +1,13 @@
 ﻿#pragma strict
 
-var startPosition : Vector3 = Vector3(-0,50,0);    //The starting position in world space
- var endPosition : Vector3 = Vector3(0,100,0);    //The ending position in world space
+
+ 
+ var startPosition : Vector3 = Vector3(0, 0, 25);    //The starting position in world space
+ var endPosition : Vector3 = Vector3(0,0,-25);    //The ending position in world space
  var bending : Vector3 = Vector3.up;                //Bend factor (on all axes)
  var timeToTravel : float = 5.0;                //The total time it takes to move from start- to end position
  
- function Start () {
+ function Update () {
      MoveToPosition();
  }
  
@@ -15,11 +17,12 @@ var startPosition : Vector3 = Vector3(-0,50,0);    //The starting position in wo
          var currentPos : Vector3 = Vector3.Lerp(startPosition, endPosition, (Time.time - timeStamp)/timeToTravel);
          
          currentPos.x += bending.x*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
-         currentPos.y += bending.y*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
+         currentPos.y += 2*Mathf.Sin(transform.position.z);
          currentPos.z += bending.z*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
          
          transform.position = currentPos;
          
          yield;
      }
+     
  }
