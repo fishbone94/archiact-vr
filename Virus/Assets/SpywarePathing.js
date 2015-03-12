1be0f1b95f83603e484e5ea4b3cc127d591e9b51
SkyZero <EdwardChao@live.com>
2015-03-12 00:03:36 -0700

    Spyware Pathing

--- Virus/Assets/SpywarePathing.js
+++ Virus/Assets/SpywarePathing.js
@@ -1,14 +1,18 @@
-﻿#pragma strict
-
-var startPosition : Vector3 = Vector3(-10,0,0);    //The starting position in world space
+﻿ var upDown : float;
+ var speed : int = 2;
+ var hightDiv : int = 150;
+ var t : float; 
+ var startPosition : Vector3 = Vector3(-10,0,0);    //The starting position in world space
  var endPosition : Vector3 = Vector3(10,0,0);    //The ending position in world space
  var bending : Vector3 = Vector3.up;                //Bend factor (on all axes)
  var timeToTravel : float = 10.0;                //The total time it takes to move from start- to end position
+ var y : float = 10.0;
  
- function Start () {
-     MoveToPosition();
+ 
+ function Start(){
+ MoveToPosition();
  }
- 
+
  function MoveToPosition () {
      var timeStamp : float = Time.time;
      while (Time.time<timeStamp+timeToTravel) {
@@ -23,3 +27,10 @@ var startPosition : Vector3 = Vector3(-10,0,0);    //The starting position in wo
          yield;
      }
  }
+
+ function Update () {
+transform.Translate(Vector3.up * upDown); 
+t += speed*(Time.deltaTime); 
+upDown = (Mathf.Sin(t))/hightDiv;
+
+}
