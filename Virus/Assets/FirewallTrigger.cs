3dd7c6adde7430b43f922ee7bbfe3f1710c11e03
Edward Chao <edwardchao@live.com>
2015-03-15 22:27:36 -0700

    Firewall in progress

---
+++ Virus/Assets/FirewallTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirewallTrigger : MonoBehaviour {
+	//Make 1-shotable
+	//When dead, run Activate();
+
+	bool dead = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (dead != false) {
+						Activate ();
+				}
+		}
+	void Activate(){
+		GameObject Firewall = Instantiate(Resources.Load("Firewall", typeof(GameObject))) as GameObject;
+		Instantiate(Firewall, new Vector3(0, 10, -15), Quaternion.Euler(0,0,0));
+	}
+	
+}
