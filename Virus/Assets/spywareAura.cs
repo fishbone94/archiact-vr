efa542c260f649c466f321936adae73f486cc0d5
vgleung <vgleung@sfu.ca>
2015-03-15 17:16:26 -0700

    completed spyware aura

---
+++ Virus/Assets/spywareAura.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spywareAura : MonoBehaviour {
+	GameObject player;
+	playerShooting playerShooting;
+
+	GameObject enemy;
+	enemyHealth enemyHealth;
+	
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerShooting = player.GetComponentInChildren<playerShooting> ();
+	}
+	
+	void Update () {
+		enemy = GameObject.FindGameObjectWithTag("Spyware");
+		enemyHealth = enemy.GetComponent <enemyHealth> ();
+		if(enemyHealth) {
+			aura();
+		}
+		if (enemyHealth.currentHealth <= 5){
+			playerShooting.damagePerShot = 20;
+		}
+	}
+
+	void aura() {
+		if (playerShooting.damagePerShot == 20) {
+			playerShooting.damagePerShot = playerShooting.damagePerShot/2;
+		}
+	}
+}
