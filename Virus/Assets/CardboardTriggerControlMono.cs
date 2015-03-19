ee0e9ab7548b92567846d18685adb26e289fac9d
SkyZero <EdwardChao@live.com>
2015-03-18 23:05:21 -0700

    magnet sensor test#1

---
+++ Virus/Assets/CardboardTriggerControlMono.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardTriggerControlMono : MonoBehaviour {
+	public bool magnetDetectionEnabled = true;
+	
+	void Start() {
+		CardboardMagnetSensor.SetEnabled(magnetDetectionEnabled);
+		// Disable screen dimming:
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		
+		
+	} 
+	
+	void Update () {
+		if (!magnetDetectionEnabled) return;
+		if (CardboardMagnetSensor.CheckIfWasClicked()) {
+			Debug.Log("Cardboard trigger was just clicked");
+			Application.LoadLevel(1);
+			CardboardMagnetSensor.ResetClick();
+			
+		}
+	}
+}
