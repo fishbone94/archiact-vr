09cf1dc6a97cc2b00ad293351695a0d4d3c88dc0
fishbone94 <danryanchan@gmail.com>
2015-03-20 02:12:45 -0700

    added score and counter

---
+++ Virus/Assets/scoreCounter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scoreCounter : MonoBehaviour {
+	
+	public static int frameCount;
+	public static int scoreCount;
+	Text text;
+	
+	void Awake() {
+		DontDestroyOnLoad (text);
+		text = GetComponent<Text> ();
+		frameCount = 0;
+		scoreCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frameCount++;
+		scoreCount = frameCount / 60;
+		text.text = "Score: " + scoreCount;
+	}
+}
