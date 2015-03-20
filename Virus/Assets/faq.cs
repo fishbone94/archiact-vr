6a8d08d286d60d881af8536d86a43e0b25345fc6
fishbone94 <danryanchan@gmail.com>
2015-03-19 18:46:14 -0700

    FAQ screen implemented and working

---
+++ Virus/Assets/faq.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class faq : MonoBehaviour {
+	enemyHealth enemyHealth;
+	GameObject start;
+	GameObject player;
+	playerShooting shot;
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		shot = player.GetComponentInChildren<playerShooting> ();
+		start = GameObject.FindGameObjectWithTag ("FAQButton");
+		enemyHealth = start.GetComponent<enemyHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enemyHealth.currentHealth <= shot.damagePerShot) {
+			Application.LoadLevel ("FAQdotexe");
+		}
+	}
+}
